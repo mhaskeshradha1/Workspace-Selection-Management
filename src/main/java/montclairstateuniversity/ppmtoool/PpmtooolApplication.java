b14b8a11f0e8986240f2a83de02c86ecf4ddacf7
shradha mhaske <mhaskeshradha1@gmail.com>
2020-10-05 21:52:41 -0400

    first commit

---
+++ src/main/java/montclairstateuniversity/ppmtoool/PpmtooolApplication.java
@@ -0,0 +1,13 @@
+package montclairstateuniversity.ppmtoool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtooolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtooolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/montclairstateuniversity/ppmtoool/PpmtooolApplicationTests.java
@@ -0,0 +1,13 @@
+package montclairstateuniversity.ppmtoool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtooolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
