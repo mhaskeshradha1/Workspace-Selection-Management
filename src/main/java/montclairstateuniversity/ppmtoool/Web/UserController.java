886807d3957526b4a68c64ac95539e51d28dbc86
Shradha Mhaske <46949105+mhaskeshradha1@users.noreply.github.com>
2020-11-18 23:15:38 -0500

    Spring Security - User Registration Password Validation

--- src/main/java/montclairstateuniversity/ppmtoool/Web/UserController.java
+++ src/main/java/montclairstateuniversity/ppmtoool/Web/UserController.java
@@ -4,6 +4,7 @@ package montclairstateuniversity.ppmtoool.Web;
 import montclairstateuniversity.ppmtoool.Services.UserService;
 import montclairstateuniversity.ppmtoool.Services.ValidationErrorService;
 import montclairstateuniversity.ppmtoool.domain.User;
+import montclairstateuniversity.ppmtoool.validator.Uservalidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +26,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private Uservalidator uservalidator;
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
         // Validate passwords match
+        uservalidator.validate(user,result);
 
         ResponseEntity<?> errorMap = ValidationErrorService.ValidationErrorService(result);
         if (errorMap != null) return errorMap;
---
+++ src/main/java/montclairstateuniversity/ppmtoool/validator/Uservalidator.java
@@ -0,0 +1,40 @@
+package montclairstateuniversity.ppmtoool.validator;
+
+import montclairstateuniversity.ppmtoool.domain.User;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+
+@Component
+public class Uservalidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object object, Errors errors) {
+
+
+
+        User user = (User) object;
+
+        if(user.getPassword().length() <6){
+            errors.rejectValue("password","Length", "Password must be at least 6 characters");
+        }
+
+        if(!user.getPassword().equals(user.getConfirmPassword())){
+            errors.rejectValue("confirmPassword","Match", "Passwords must match");
+
+        }
+
+        //confirmPassword
+
+
+
+
+
+    }
+}
